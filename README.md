e0d3a36d2a973591a8455fd0bd19bf67fee6c71b
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-09-20 22:30:02 +0700

    :wrench:: Refactor code and update README

--- README.md
+++ README.md
@@ -9,3 +9,35 @@ To run the project:
 - Clone or fork
 - Go to terminal type: `npm start`
 - Enjoy!
+
+# Những thứ đã học được sau khi làm project này
+
+1. Cách xử lí khi có nhiều api giống nhau, ta sẽ tạo ra 1 file config api riêng, tạo object chứa các function để get ra api trong 1 file riêng nếu có quá nhiều components sử dụng các api khác nhau
+   <br>
+
+```js
+export const apiKey = "3ce49afbabd14f11e4b7097cf42c2ab9";
+const tmdbEndpoint = "https://api.themoviedb.org/3/movie";
+const tmdbEndpointSearch = "https://api.themoviedb.org/3/search/movie";
+export const tmdbAPI = {
+  getMovieList: (type, page = 1) =>
+    `${tmdbEndpoint}/${type}?api_key=${apiKey}&page=${page}`,
+  getMovieSearch: (query, page) =>
+    `${tmdbEndpointSearch}?api_key=3ce49afbabd14f11e4b7097cf42c2ab9&language=en-US&query=${query}&page=${page}`,
+  getMovieDetails: (movieId) => `${tmdbEndpoint}/${movieId}?api_key=${apiKey}`,
+  getMovieMeta: (type, movieId) =>
+    `${tmdbEndpoint}/${movieId}/${type}?api_key=${apiKey}`,
+  imageOriginal: (url) => `https://image.tmdb.org/t/p/original${url}`,
+};
+```
+
+2. Cách sử dụng lazy và Suspense để chia ra thành nhiều chunk nhỏ khác nhau, để khi load web, web sẽ không phải load tất cả component một lúc, mà vào trang nào thì mới load tới đó nhưng vẫn giữ tính chất SPA
+   <br>
+
+3. Cách sử dụng SWR để get data từ api
+
+4. Cách phân trang sử dụng react-paginate
+
+5. Tìm hiểu về sự khác nhau giữa HashRouter và BrowserRouter
+
+6. Prop-types và Error Boundaries
--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -45,100 +45,92 @@ const MovieDetailsPage = () => {
       <p className="text-[19px] mb-10 text-center leading-loose max-w-[700px] mx-auto text-white">
         {overview}
       </p>
-      <MovieCredits></MovieCredits>
-      <MovieVideos></MovieVideos>
-      <MoviesSimilar></MoviesSimilar>
+      <MovieMeta type="credits"></MovieMeta>
+      <MovieMeta type="videos"></MovieMeta>
+      <MovieMeta type="similar"></MovieMeta>
     </>
   );
 };
 
-function MovieCredits() {
+function MovieMeta({ type = "videos" }) {
   const { movieId } = useParams();
-  const { data } = useSWR(tmdbAPI.getMovieMeta("credits", movieId), fetcher);
-  //https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=3ce49afbabd14f11e4b7097cf42c2ab9
-  // console.log("log ~ MovieCredits ~ data", data);
+  const { data } = useSWR(tmdbAPI.getMovieMeta(type, movieId), fetcher);
   if (!data) return null;
-  const { cast } = data;
-  if (!cast || cast.length < 0) return;
-  return (
-    <div className="text-white">
-      <h1 className="text-center text-white font-medium text-[36px] mb-10">
-        Credits
-      </h1>
-      <div className="grid grid-cols-4 gap-5 px-10">
-        {cast.length > 0 &&
-          cast.slice(0, 8).map((item) => (
-            <div key={item.id} className="cast-item">
-              <img
-                src={tmdbAPI.imageOriginal(item.profile_path)}
-                alt=""
-                className="w-full h-[350px] object-cover rounded-lg mb-5"
-              />
-              <h3 className="text-xl font-medium text-center">{item.name}</h3>
-            </div>
-          ))}
-      </div>
-    </div>
-  );
-}
-
-function MovieVideos() {
-  const { movieId } = useParams();
-  const { data } = useSWR(tmdbAPI.getMovieMeta("videos", movieId), fetcher);
-  if (!data || !data.results) return null;
-  const { results } = data;
-  if (!results || results.length < 0) return null;
-  return (
-    <>
-      <h1 className="text-center text-white font-medium text-[36px] mb-10">
-        Teaser & Trailers
-      </h1>
-      <div className="grid items-center grid-cols-3 gap-10 text-white justify-items-center">
-        {results.slice(0, 6).map((item) => (
-          <div key={item.id} className="mb-10">
-            <iframe
-              width="557"
-              height="370"
-              src={`https://www.youtube.com/embed/${item.key}`}
-              title={item.name}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        ))}
-      </div>
-    </>
-  );
-}
-
-function MoviesSimilar() {
-  const { movieId } = useParams();
-  const { data } = useSWR(tmdbAPI.getMovieMeta("similar", movieId), fetcher);
-  if (!data || !data.results) return null;
-  const { results } = data;
-  if (!results || results.length < 0) return null;
-  return (
-    <div className="py-10 mx-auto text-center text-white">
-      <h2 className="mb-10 text-3xl font-medium">Similar Movies</h2>
-      <div className="movie-list">
-        <Swiper
-          freeMode={true}
-          navigation={true}
-          grabCursor={true}
-          slidesPerView={"auto"}
-          spaceBetween={40}
-          modules={[FreeMode, Navigation]}
-        >
-          {results.length > 0 &&
-            results.map((item) => (
-              <SwiperSlide key={item.id}>
-                <MovieCard item={item}></MovieCard>
-              </SwiperSlide>
+  if (type === "credits") {
+    const { cast } = data;
+    console.log("log ~ MovieMeta ~ cast", cast);
+    if (!cast || cast.length < 0) return null;
+    return (
+      <div className="text-white">
+        <h1 className="text-center text-white font-medium text-[36px] mb-10">
+          Credits
+        </h1>
+        <div className="grid grid-cols-4 gap-5 px-10">
+          {cast.length > 0 &&
+            cast.slice(0, 8).map((item) => (
+              <div key={item.id} className="cast-item">
+                <img
+                  src={tmdbAPI.imageOriginal(item.profile_path)}
+                  alt=""
+                  className="w-full h-[350px] object-cover rounded-lg mb-5"
+                />
+                <h3 className="text-xl font-medium text-center">{item.name}</h3>
+              </div>
             ))}
-        </Swiper>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const { results } = data;
+    if (!results || results.length < 0) return null;
+    if (type === "videos")
+      return (
+        <>
+          <h1 className="text-center text-white font-medium text-[36px] mb-10">
+            Teaser & Trailers
+          </h1>
+          <div className="grid items-center grid-cols-3 gap-10 text-white justify-items-center">
+            {results.slice(0, 6).map((item) => (
+              <div key={item.id} className="mb-10">
+                <iframe
+                  width="557"
+                  height="370"
+                  src={`https://www.youtube.com/embed/${item.key}`}
+                  title={item.name}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            ))}
+          </div>
+        </>
+      );
+    if (type === "similar")
+      return (
+        <div className="py-10 mx-auto text-center text-white">
+          <h2 className="mb-10 text-3xl font-medium">Similar Movies</h2>
+          <div className="movie-list">
+            <Swiper
+              freeMode={true}
+              navigation={true}
+              grabCursor={true}
+              slidesPerView={"auto"}
+              spaceBetween={40}
+              modules={[FreeMode, Navigation]}
+            >
+              {results.length > 0 &&
+                results.map((item) => (
+                  <SwiperSlide key={item.id}>
+                    <MovieCard item={item}></MovieCard>
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+          </div>
+        </div>
+      );
+    return null;
+  }
 }
+
 export default MovieDetailsPage;
