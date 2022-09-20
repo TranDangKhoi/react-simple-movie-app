45dba6fce5af413794863a6d078721ffaf632c17
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-09-20 16:35:58 +0700

    :100:: Learn about splitting routes, chunks and apply it to this project

--- src/App.js
+++ src/App.js
@@ -3,33 +3,39 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/bundle";
 import Banner from "./components/banner/Banner";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/layout/Main";
-import Homepage from "./pages/Homepage";
-import MoviePage from "./pages/MoviePage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
+// import Homepage from "";
+// import MoviePage from "";
+// import MovieDetailsPage from "";
 
+const Homepage = lazy(() => import("./pages/Homepage"));
+const MoviePage = lazy(() => import("./pages/MoviePage"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
 function App() {
   return (
     <>
-      <Routes>
-        <Route element={<Main></Main>}>
-          <Route
-            path="/"
-            element={
-              <>
-                <Banner></Banner>
-                <Homepage></Homepage>
-              </>
-            }
-          ></Route>
-          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
-          <Route
-            path="/movie/:movieId"
-            element={<MovieDetailsPage></MovieDetailsPage>}
-          ></Route>
-        </Route>
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Banner></Banner>
+                  <Homepage></Homepage>
+                </>
+              }
+            ></Route>
+            <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+            <Route
+              path="/movie/:movieId"
+              element={<MovieDetailsPage></MovieDetailsPage>}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 }
